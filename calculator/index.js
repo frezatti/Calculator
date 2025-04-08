6373b34501d61bbbf1627cfd1f932f9ad8b71b84
Adriel Frezatti <Adrielfrezatti@gmail.com>
2025-04-08 20:28:17 -0300

    Coffee is Heregit

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,72 @@
-let numbers = 0;
+let calculator = document.querySelector(".calculator");
+let screen = document.querySelector(".screen");
+let buffer = 1;
+let operator = "+";
+let first = true;
+let old = "";
+
+calculator.addEventListener("click", function(event) {
+    input = event.target.innerText;
+
+    if (isNaN(parseInt(input))) {
+        old = operator;
+        operator = input;
+        computeResult();
+    }
+    else {
+        updateScreen(input);
+    }
+});
+
+function updateScreen(number) {
+    if (screen.innerText == "0") {
+        screen.innerText = number.toString();
+    }
+    else {
+        screen.innerText += number.toString();
+    }
+}
+
+function computeResult() {
+    let number = parseInt(screen.innerText);
+    console.log(`number that was on the screen: ${number} `);
+    console.log(` buffer before operations: ${buffer}`);
+    switch (operator) {
+        case "+":
+            if (first) {
+                buffer--;
+                first = false;
+            }
+            buffer += number;
+            screen.innerText = "0";
+            break;
+
+        case "-":
+            if (first) {
+                buffer--;
+                first = false;
+            }
+            buffer = buffer - number
+            screen.innerText = "0";
+            break;
+
+        case "x":
+            buffer = number * buffer;
+            screen.innerText = "0";
+            break;
+        case "÷":
+            break;
+        case "C":
+            screen.innerText = "0";
+            buffer = 1;
+            first = 1;
+            operator = "+"
+            break;
+        default:
+            operator = old;
+            computeResult();
+            screen.innerText = buffer.toString();
+            break;
+    }
+    console.log(` buffer after operations: ${buffer}`);
+}
